f016457d427a15a7284cd21d02eb4f50be13b135
Zheng Hao Tan <tanzhao@umich.edu>
2015-07-12 23:40:57 -0500

    Added PageView controllers for tutorial screens and login/signup view controllers

--- summerproject/summerproject/Models/UserProfile.swift
+++ summerproject/summerproject/Models/UserProfile.swift
@@ -7,6 +7,47 @@
 //
 
 import Foundation
+import Parse
 
-// TODO: Add UserProfilestruct
+protocol UserProfileProtocol {
+
+    // REQUIRES: A user profile.
+    // EFFECTS: Returns true if user is created successfully, false otherwise.
+    func addUser(userProfile: String) -> Bool
+    
+    
+    func editUser(UserProfile: String) -> Bool
+    
+    // REQUIRES: A valid user.
+    // EFFECTS: Returns true if user is successfully deleted, false otherwise.
+    func removeUser(userProfile: String) -> Bool
+}
+
+class UserProfile : UserProfileProtocol {
+    
+    func addUser(userProfile: String) -> Bool {
+        
+        let userProfile = PFObject(className: userProfile as String)
+        userProfile["biography"] = "Hi, I am biography."
+        userProfile["rewardPoints"] = 10
+        
+        userProfile.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            println("User Profile has been saved.")
+        }
+        return true
+    }
+    
+    func editUser(UserProfile: String) -> Bool {
+        
+        return true
+    }
+    
+    
+    func removeUser(userProfile: String) -> Bool {
+        return true
+    }
+}
+
+
+
 
