acb501035b3b76f87a0c8a4ed1e1ef7e9e97823c
Tommy Fang <tfang17@stanford.edu>
2015-07-14 00:08:07 -0500

    fixed

---
+++ summerproject/summerproject/Models/Reviews/Reviews.swift
@@ -0,0 +1,14 @@
+//
+//  Reviews.swift
+//  summerproject
+//
+//  Created by Zheng Hao Tan on 7/13/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+class Reviews : ReviewsProtocol {
+    
+    
+}
