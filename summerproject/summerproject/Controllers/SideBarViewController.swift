5e94a5aa117e24b8d37d772b7f5d1830c5b3890e
Tommy Fang <tfang17@stanford.edu>
2015-07-14 22:38:49 -0500

    side bar additions

---
+++ summerproject/summerproject/Controllers/SideBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SideBarTableViewController.swift
+//  summerproject
+//
+//  Created by Tommy Fang on 7/14/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class SideBarTableViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
