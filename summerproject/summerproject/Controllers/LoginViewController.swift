d86bfa9d237847e3259434ff1ea77a29a5f7cbd2
Tommy Fang <tfang17@stanford.edu>
2015-07-19 21:55:07 -0500

    0 warnings

--- summerproject/summerproject/Controllers/LoginViewController.swift
+++ summerproject/summerproject/Controllers/LoginViewController.swift
@@ -24,7 +24,6 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
--- summerproject/summerproject/Controllers/PageViewController.swift
+++ summerproject/summerproject/Controllers/PageViewController.swift
@@ -25,7 +25,6 @@ class PageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     /*
     // MARK: - Navigation
     
