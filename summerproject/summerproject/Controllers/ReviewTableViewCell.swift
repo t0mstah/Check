7f83eb09df8fb502c2e9cd8d3a1d886336b35ab6
Tommy Fang <tfang17@stanford.edu>
2015-07-27 22:25:22 -0500

    profile page front-end

---
+++ summerproject/summerproject/Controllers/ReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewTableViewCell.swift
+//  Check
+//
+//  Created by Tommy Fang on 7/27/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
