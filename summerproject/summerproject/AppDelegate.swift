bf928f01a90521f98e12c3edaebd82b26c539547
Zheng Hao Tan <tanzhao@umich.edu>
2015-07-28 22:51:30 -0500

    added bridging header to fb logins

--- summerproject/summerproject/AppDelegate.swift
+++ summerproject/summerproject/AppDelegate.swift
@@ -32,7 +32,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // [Optional] Track statistics around application opens.
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
         return true
+    }
+
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?,
+                    annotation: AnyObject?) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(application,
+                openURL: url,
+                sourceApplication: sourceApplication,
+                annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -51,6 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
